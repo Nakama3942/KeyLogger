44086d4b6ae028a7a1edfba7c235949f8ca3fbf3
Kalynovsky Valentin <nakama3942@gmail.com>
2022-10-12 16:02:50 +0300

    Started moving from QKeyEvent and QMouseEvent to pynput.keyboard and pynput.mouse

--- ui/keylogger.py
+++ ui/keylogger.py
@@ -17,6 +17,7 @@
 # from time import sleep
 # import pickle
 # import os
+from pynput import keyboard, mouse
 
 # import PyQt6
 from PyQt6 import QtWidgets
@@ -27,10 +28,34 @@ from PyQt6.QtWidgets import QApplication,\
                             QStyle,\
                             QMenu
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QAction, QKeyEvent
-# from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtGui import QAction
+from PyQt6.QtCore import QThread, pyqtSignal
 
 from ui.raw.ui_keylogger import Ui_KeyLogger
+
+
+class ButtonManager(QThread):
+    clicked = pyqtSignal(str)
+
+    def __init__(self):
+        super(ButtonManager, self).__init__()
+        self._keyboardListener = keyboard.Listener(on_press=self._keyboard_click)
+        self._mouseListener = mouse.Listener(on_click=self._mouse_click)
+
+    def run(self):
+        self._keyboardListener.start()
+        self._mouseListener.start()
+
+    def _keyboard_click(self, key):
+        print("Key pressed: {0}".format(key))
+        self.clicked.emit(str(key))
+
+    def _mouse_click(self, x, y, button, pressed):
+        if pressed:
+            print('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
+            self.clicked.emit(str(button))
+        else:
+            print('Mouse released at ({0}, {1}) with {2}'.format(x, y, button))
 
 
 class KeyLogger(QMainWindow, Ui_KeyLogger):
@@ -50,6 +75,10 @@ class KeyLogger(QMainWindow, Ui_KeyLogger):
         tray_menu.addAction(show_action)
         self.tray_icon.setContextMenu(tray_menu)
 
+        self.button_manager = ButtonManager()
+        self.button_manager.start()
+        self.button_manager.clicked.connect(self.displayer)
+
     def tray_Show(self):
         self.tray_icon.hide()
         self.show()
@@ -58,8 +87,17 @@ class KeyLogger(QMainWindow, Ui_KeyLogger):
         self.tray_icon.show()
         self.hide()
 
-    def keyPressEvent(self, event: QKeyEvent):
-        for item in Qt.Key:
-            if event.key() == item:
-                self.textBrowser.append(str(item))
-                break
+    def displayer(self, button: str):
+        self.textBrowser.append(button)
+
+    # def keyPressEvent(self, event: QKeyEvent):
+    #     for item in Qt.Key:
+    #         if event.key() == item:
+    #             self.textBrowser.append(str(item.name))
+    #             break
+    #
+    # def mousePressEvent(self, event: QMouseEvent):
+    #     for item in Qt.MouseButton:
+    #         if event.button() == item:
+    #             self.textBrowser.append(str(item.name))
+    #             break
