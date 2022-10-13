b5d553662b765b11df061efe9ebdcc488f4944db
Kalynovsky Valentin <nakama3942@gmail.com>
2022-10-13 19:50:15 +0300

    Added functions for converting RGB to other color schemes; added Sepia color scheme

--- src/color_scheme.py
+++ src/color_scheme.py
@@ -1,35 +1,21 @@
-schemes = [{'GUI': 'Standard', 'key_clicked': '006400', 'mouse_clicked': '646200', 'mouse_released': '002a64', 'other_data': '503070'},
-           {'GUI': 'Monochrome', 'key_clicked': '3b3b3b', 'mouse_clicked': '585858', 'mouse_released': '242424', 'other_data': '414141'}]
-
-
-def rgb_to_mono(color: str, gray: bool = True):
-    # Source: https://www.had2know.org/technology/rgb-to-gray-scale-converter.html
-    # Algorithm:
-    # print(f'RGB:  {color}')
-    # red = color[0:2]
-    # green = color[2:4]
-    # blue = color[4:6]
-    # if gray:
-    #     mono_red = int(red, 16) * 0.299
-    #     mono_green = int(green, 16) * 0.587
-    #     mono_blue = int(blue, 16) * 0.114
-    #
-    #     mono_color = hex(round(mono_red + mono_green + mono_blue)).replace('0x', '').zfill(2)
-    #     mono_color = mono_color * 3
-    # else:
-    #     mono_red = hex(round(int(red, 16) * 0.299)).replace('0x', '').zfill(2)
-    #     mono_green = hex(round(int(green, 16) * 0.587)).replace('0x', '').zfill(2)
-    #     mono_blue = hex(round(int(blue, 16) * 0.114)).replace('0x', '').zfill(2)
-    #
-    #     mono_color = mono_red + mono_green + mono_blue
-    # print(f'MONO: {mono_color}')
-    # return mono_color
-    if gray:
-        return (hex(round((int(color[0:2], 16) * 0.299) + (int(color[2:4], 16) * 0.587) + (int(color[4:6], 16) * 0.114))).replace('0x', '').zfill(2)) * 3
-    else:
-        return hex(round(int(color[0:2], 16) * 0.299)).replace('0x', '').zfill(2) + hex(round(int(color[2:4], 16) * 0.587)).replace('0x', '').zfill(2) + hex(round(int(color[4:6], 16) * 0.114)).replace('0x', '').zfill(2)
-
-
-# Converting colors
-if __name__ == '__main__':
-    print(rgb_to_mono('503070', False))
+schemes = [
+    {
+        'GUI': 'Standard',
+        'key_clicked': '006400',
+        'mouse_clicked': '646200',
+        'mouse_released': '002a64',
+        'other_data': '503070'
+    }, {
+        'GUI': 'Monochrome',
+        'key_clicked': '3b3b3b',
+        'mouse_clicked': '585858',
+        'mouse_released': '242424',
+        'other_data': '414141'
+    }, {
+        'GUI': 'Sepia',
+        'key_clicked': '4d4535',
+        'mouse_clicked': '736650',
+        'mouse_released': '332e24',
+        'other_data': '5a503e'
+    }
+]
---
+++ src/rgb_converter.py
@@ -0,0 +1,47 @@
+def rgb_to_mono(color: str, gray: bool = True):
+    # Source: https://www.had2know.org/technology/rgb-to-gray-scale-converter.html
+    # Algorithm:
+    # print(f'RGB:  {color}')
+    # red = color[0:2]
+    # green = color[2:4]
+    # blue = color[4:6]
+    # if gray:
+    #     mono_red = int(red, 16) * 0.299
+    #     mono_green = int(green, 16) * 0.587
+    #     mono_blue = int(blue, 16) * 0.114
+    #
+    #     mono_color = hex(round(mono_red + mono_green + mono_blue)).replace('0x', '').zfill(2)
+    #     mono_color = mono_color * 3
+    # else:
+    #     mono_red = hex(round(int(red, 16) * 0.299)).replace('0x', '').zfill(2)
+    #     mono_green = hex(round(int(green, 16) * 0.587)).replace('0x', '').zfill(2)
+    #     mono_blue = hex(round(int(blue, 16) * 0.114)).replace('0x', '').zfill(2)
+    #
+    #     mono_color = mono_red + mono_green + mono_blue
+    # print(f'MONO: {mono_color}')
+    # return mono_color
+    if gray:
+        return (hex(round((int(color[0:2], 16) * 0.299) + (int(color[2:4], 16) * 0.587) + (int(color[4:6], 16) * 0.114))).replace('0x', '').zfill(2)) * 3
+    else:
+        return hex(round(int(color[0:2], 16) * 0.299)).replace('0x', '').zfill(2) + hex(round(int(color[2:4], 16) * 0.587)).replace('0x', '').zfill(2) + hex(round(int(color[4:6], 16) * 0.114)).replace('0x', '').zfill(2)
+
+
+def rgb_to_sepia(color: str):
+    # print(f'RGB:  {color}')
+    red = color[0:2]
+    green = color[2:4]
+    blue = color[4:6]
+
+    sepia_red = hex(round((int(red, 16) * 0.393) + (int(green, 16) * 0.769) + (int(blue, 16) * 0.189))).replace('0x', '').zfill(2)
+    sepia_green = hex(round((int(red, 16) * 0.349) + (int(green, 16) * 0.686) + (int(blue, 16) * 0.168))).replace('0x', '').zfill(2)
+    sepia_blue = hex(round((int(red, 16) * 0.272) + (int(green, 16) * 0.534) + (int(blue, 16) * 0.131))).replace('0x', '').zfill(2)
+
+    sepia_color = sepia_red + sepia_green + sepia_blue
+    # print(f'MONO: {sepia_color}')
+    return sepia_color
+
+
+# Converting colors
+if __name__ == '__main__':
+    # print(rgb_to_mono('646200', False))
+    print(rgb_to_sepia('503070'))
